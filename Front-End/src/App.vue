afb0eef724da2da4cf8b65b297e3523e17ec1cf3
dongh810 <baek0810@naver.com>
2024-04-05 17:48:34 +0900

    header & footer 구현

---
+++ Front-End/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import Header from "@/components/Header/Login_Header.vue";
+    import Footer from "@/components/Footer/Footer.vue";
+    import Login from "@/components/Login/Login.vue";
+</script>
+
+<template>
+        <Header></Header>
+
+        <Login></Login>
+
+        <Footer></Footer>  
+</template>
+
+<style scoped>
+</style>
---
+++ Front-End/src/components/Header/Login_Header.vue
@@ -0,0 +1,101 @@
+<template>
+    <header class="header">
+        <div class="logo">
+            <img src="../../assets/ESC_logo.png" class="logoimage">
+        </div>
+        <div class="menu">
+            <span>소개</span>
+            <span>모집</span>
+            <span>스터디클럽</span>
+            <span>마이페이지</span>
+        </div>
+        <div class="loginbtndiv">
+            <button type="button" class="logoutBtn" onclick="">logout</button>
+            <button type="button" class="myPageBtn" onclick="">my page</button>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+     * {
+		margin: 0;
+		padding: 0;
+        font-family: 'GmarketSansMedium';
+	}
+
+    .header {
+        width: 100%;
+        height: 100px;
+        position: relative;
+        top: 0;
+        background-color: white;
+        display: grid;
+        grid-template-columns: 1fr 2fr 1fr;
+        justify-items: center;
+    }
+
+    .loginbtndiv {
+        width:100%;
+        text-align: end;
+    }
+
+    .logoutBtn {
+        background-color: #515050;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 12px;
+        font-style: bold;
+        margin-top:53px;
+        margin-right: 2%;
+        
+        
+    }
+
+    .myPageBtn {
+        background-color: #515050;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        font-size: 12px;
+        font-style: bold;
+        margin-top:53px;    
+        margin-right: 2%;
+    }
+
+    .logoimage {
+        width:100%;
+        object-fit: contain;
+    }
+
+    .logo {
+        width:300px;
+        cursor: pointer;
+        margin-top: 2.5%;
+        margin-left: 2%;
+        justify-self: start;
+    }
+
+    .menu {
+        text-align: center;
+        margin-top:65px;
+        font-weight: bold;
+        margin-right:2%;
+        width: 100%;
+        
+    }
+    .menu span{
+        margin-left: 3%;
+        cursor: pointer;
+    }
+
+        
+</style>
