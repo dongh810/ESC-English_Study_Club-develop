d57a79e2bbd93b4031058e6dff4d7c7873a87725
dongh810 <baek0810@naver.com>
2024-04-09 10:24:22 +0900

    Feat: 로그인 테스트중

--- Front-End/src/components/Login/Login.vue
+++ Front-End/src/components/Login/Login.vue
@@ -6,11 +6,11 @@
             </div>
             <div class="idTitle">
                 <h2>아이디</h2>
-                <input type="text" class="idBox" placeholder="아이디를 입력하세요" v-model="id">
+                <input type="text" class="idBox" placeholder="아이디를 입력하세요" v-model.trim="member_id">
             </div>
             <div class="pwdTitle">
                 <h2>비밀번호</h2>
-                <input type="password" class="pwdBox" placeholder="비밀번호를 입력하세요" v-model="pwd">
+                <input type="password" class="pwdBox" placeholder="비밀번호를 입력하세요" v-model.trim="member_pwd">
             </div>
             <div>
                 <span class="left">회원가입</span>
@@ -26,7 +26,50 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+const member_id = ref('');
+const member_pwd = ref('');
 
+console.log(member_id);
+console.log(member_pwd);
+
+const tokenData = async () => {
+    await axios.post("http://localhost:5173/api/login",
+    {
+        memberId: member_id.value,
+        memberPwd: member_pwd.value
+    }).then ((response) => {    // then: post 요청 성공 시 동작할 콜백 함수 등록
+        if(response.status == 200) {
+            console.log('response status: ', response.status);
+            console.log('response headers: ', response.headers);
+
+            // 토큰 및 아이디 로컬 스토리지에 저장
+            localStorage.setItem('token', response.headers.token)
+            localStorage.setItem('member_id', member_id.value)
+
+            router.push('/');
+        }
+    }).catch ((e) => {
+        console.log('로그인 실패');
+        alert('입력한 정보가 유효하지 않습니다. 아이디와 비밀번호를 확인해주세요.');
+    })
+};
+
+function inputCheck() {
+    if(member_id.value == ''){
+        alert('아이디를 입력해주세요.');
+        return false;
+    } else if (member_pwd.value == ''){
+        alert('비밀번호를 입력해주세요.');
+        return false;
+    } else {
+        // 서버 연동하여 토큰값 가져온 후 유효성 검사 코드 추가
+        console.log('입력 정보 확인 완료');
+        console.log(member_id.value);
+        console.log(member_pwd.value);
+        return true;
+    }
+}
 </script>
 
 <style scoped>
